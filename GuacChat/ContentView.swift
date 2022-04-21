91f7d9a1f0b62f78569f904321bb9841e5b19483
Ryan Downs <rj_downs@yahoo.com>
2022-04-21 00:59:28 -0700

    Initial Commit

---
+++ GuacChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuacChat
+//
+//  Created by Ryan Downs on 4/21/22.
+//  Copyright © 2022 Ryan Downs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
